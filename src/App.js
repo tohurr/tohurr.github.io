23d1e797f04257e1e217cca1cbd12c746c85f9ed
Tohur Rahman <tohurrahman@Tohurs-MBP.home>
2022-03-01 07:09:01 -0500

    modified App.js to say {insert your name here}

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My name is Tohur Rahman and I'm going to master React
         </p>
         <a
           className="App-link"
