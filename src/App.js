0f0cfa5de1718f5dc418802f16826b0eb592c49f
Tohur Rahman <tohurrahman@Tohurs-MBP.home>
2022-03-01 09:01:42 -0500

    final commit, I will become a React pro

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/tohurr"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check Me Out!
         </a>
       </header>
     </div>
