45c55ce5c6f960e3b416ac5b0b5627476583890c
Tohur Rahman <tohurrahman@Tohurs-MBP.home>
2022-12-19 18:40:01 -0500

    Adding React files

--- README.md
+++
@@ -1,3 +0,0 @@
-# My Online Portfolio
-
-### If you would like to go to my website please click [here](https://tohurr.github.io/).
---
+++ src/components/LandingSection.js
@@ -0,0 +1,40 @@
+import React from "react"; 
+import { Avatar, Heading, VStack } from "@chakra-ui/react"; 
+import FullScreenSection from "./FullScreenSection"; 
+ 
+const greeting = "Hello, I am Tohur!"; 
+const bio1 = "A Frontend Developer"; 
+// const bio2 = "specialized in React"; 
+ 
+const LandingSection = () => ( 
+ <FullScreenSection 
+   justifyContent="center" 
+   alignItems="center" 
+   isDarkBackground 
+   backgroundColor="#2A4365" 
+   //"#2A4365" 
+ > 
+   <VStack spacing={16}> 
+     <VStack spacing={4} alignItems="center"> 
+       <Avatar 
+        //  src="" 
+         size="2xl" 
+         name="Tohur Rahman" 
+       /> 
+       <Heading as="h4" size="md" noOfLines={1} color="#C0D7FB"> 
+         {greeting} 
+       </Heading> 
+     </VStack> 
+     <VStack spacing={6}> 
+       <Heading as="h1" size="3xl" noOfLines={1} color="#C0D7FB"> 
+         {bio1} 
+       </Heading> 
+       {/* <Heading as="h1" size="3xl" noOfLines={1}> 
+         {bio2} 
+       </Heading>  */}
+     </VStack> 
+   </VStack> 
+ </FullScreenSection> 
+); 
+ 
+export default LandingSection;
